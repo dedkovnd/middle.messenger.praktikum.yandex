14a0b6715459a6c501e13f571f8c2748e33fc6b2
dedkovnd <dedkovnd@gmail.com>
2024-07-15 03:15:26 +0300

    fix4

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
 
-app.use(express.static(`./dist`));
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port http://localhost:${PORT}`);
-});
+app.use(express.static(path.join(__dirname, './dist')))
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, './dist/index.html'))
+})
+
+app.listen(3000, () => {
+  console.log('Server started at port http://localhost:3000')
+})

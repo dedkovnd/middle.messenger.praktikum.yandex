914b90ca2cb1f7b89e5fb03874868bce866ff0c5
dedkovnd <dedkovnd@gmail.com>
2024-08-11 19:11:11 +0300

    fixed package scripts

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,5 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {languageOptions: { globals: globals.browser }}
 ];
